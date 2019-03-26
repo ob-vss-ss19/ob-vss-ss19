7ef75b0d5279ce9277af10cc2948fc0dff2653b1
Oliver Braun <ob@cs.hm.edu>
2019-03-26 12:12:58 +0100

    Stand Vorlesung

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/ob-vss-ss19/ob-vss-ss19
+
+go 1.12
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"github.com/ob-vss-ss19/ob-vss-ss19/stringutil"
+)
+
+func main() {
+	s := "Hallo Welt!"
+	fmt.Println(stringutil.Reverse(s))
+}
